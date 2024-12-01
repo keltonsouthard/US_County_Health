8fb96f29ef409f898c31b19e63d6702d2f21eb00
Kelton Southard <msouthard6@gatech.edu>
2024-12-01 12:08:55 -0500

    collinearity, nan, and nonlinearity analysis

--- data/EDA.py
+++ data/EDA.py
@@ -10,17 +10,17 @@ import seaborn as sns
 """
 Load datasets
 """
-## FoodEnvironmentAtlas
-variable_list = pd.read_csv('data/FoodEnvironmentAtlas/VariableList.csv')
-supp_data_state = pd.read_csv('data/FoodEnvironmentAtlas/SupplementalDataState.csv')
-supp_data_county = pd.read_csv('data/FoodEnvironmentAtlas/SupplementalDataCounty.csv')
-state_county_data = pd.read_csv('data/FoodEnvironmentAtlas/StateAndCountyData.csv')
-
-## HeartDiseaseMortality
-heart_disease_mortality = pd.read_csv('data/HeartDiseaseMortality/Heart_Disease_Mortality.csv')
-
-## LifeExpectancy
-life_expectancy = pd.read_csv('data/LifeExpectancy/U.S._Life_Expectancy.csv')
+# ## FoodEnvironmentAtlas
+# variable_list = pd.read_csv('data/FoodEnvironmentAtlas/VariableList.csv')
+# supp_data_state = pd.read_csv('data/FoodEnvironmentAtlas/SupplementalDataState.csv')
+# supp_data_county = pd.read_csv('data/FoodEnvironmentAtlas/SupplementalDataCounty.csv')
+# state_county_data = pd.read_csv('data/FoodEnvironmentAtlas/StateAndCountyData.csv')
+#
+# ## HeartDiseaseMortality
+# heart_disease_mortality = pd.read_csv('data/HeartDiseaseMortality/Heart_Disease_Mortality.csv')
+#
+# ## LifeExpectancy
+# life_expectancy = pd.read_csv('data/LifeExpectancy/U.S._Life_Expectancy.csv')
 
 """
 Objective 1 feature selection
@@ -44,7 +44,7 @@ obj1_df.drop(supp_data_county.columns[1:], axis=1, inplace=True)
 
 ## Merge heart disease mortality
 hdm = pd.read_csv('data/HeartDiseaseMortality/Heart_Disease_Mortality.csv')
-hdm = hdm.loc[(heart_disease_mortality["Stratification1"]=="Overall") & (hdm["Stratification2"]=="Overall"), ["LocationAbbr", "LocationDesc", "Data_Value"]]
+hdm = hdm.loc[(hdm["Stratification1"]=="Overall") & (hdm["Stratification2"]=="Overall"), ["LocationAbbr", "LocationDesc", "Data_Value"]]
 hdm.rename(columns={'LocationAbbr': 'State', 'LocationDesc': 'County', 'Data_Value': 'HDM'}, inplace=True)
 hdm['County'] = [c if 'County' not in c else c[:-7] for c in hdm['County']]
 hdm['CountySt'] = hdm['County'].str.cat(hdm['State'], sep=', ')
@@ -58,7 +58,6 @@ life_expectancy.dropna(axis=0, how='any', inplace=True)
 life_expectancy['County'] = life_expectancy['County'].str.replace(' County', '')
 life_expectancy.rename(columns={'County': 'CountySt'}, inplace=True)
 life_expectancy = life_expectancy[['CountySt','Life Expectancy']].groupby(['CountySt']).mean()
-life_expectancy.set_index('CountySt', inplace=True)
 obj1_df = obj1_df.join(life_expectancy, how='left')
 
 ## Remove duplicated indices
@@ -92,9 +91,53 @@ obj1_pca['PCA'] = response_pca[:,0]
 sns.pairplot(obj1_pca, corner=True, diag_kind='kde', kind='kde')
 plt.show()
 
+plt.figure(figsize=(10,8))
 sns.heatmap(obj1_pca.corr(), annot=True)
 plt.show()
 
 # count nans in response variables
 obj1_df[['HDM', 'Life Expectancy', 'PCT_DIABETES_ADULTS13']].isna().sum()
 print(f'% data retained after removing response variable nans: {obj1_pca.shape[0] / obj1_df.shape[0]*100:.2f}')
+
+## Collinearity
+corr = obj1_df.corr().abs().unstack()
+corr = corr.sort_values(ascending=False)
+print('\n'.join([f'{c1}, {c2}: {corr[c1,c2]:.4f}' for c1, c2 in corr.index if c1 != c2 and corr[c1,c2] > 0.7]))
+
+# drop collinear variables
+obj1_df.drop(['CHIPSTAX_STORES14', 'PCT_LACCESS_CHILD15', 'PCT_LACCESS_SENIORS15', 'PCT_LACCESS_LOWI15', 'PC_SNAPBEN12'], axis=1, inplace=True)
+
+## Nan analysis
+# count nans in all variables
+nan_count = obj1_df.isna().sum()
+print(nan_count[nan_count > 0.05*obj1_df.shape[0]])
+
+# drop variables with >5% nans (except census population and life expectancy)
+obj1_df.drop(['AGRITRSM_RCT12', 'BERRY_ACRES12', 'DIRSALES12', 'ORCHARD_ACRES12', 'PC_DIRSALES12', 'VEG_ACRES12'], axis=1, inplace=True)
+
+# % counties with nans
+print(f'counties without nans = {obj1_df.dropna(axis=0).shape[0]}, {obj1_df.dropna(axis=0).shape[0]/obj1_df.shape[0]*100:.2f}%')
+
+## Check if counties with nans have worse health
+nan_count_per_row = obj1_df.isna().sum(axis=1)
+nan_df = pd.DataFrame(nan_count_per_row).join(obj1_df['PCT_DIABETES_ADULTS13'], how='left')
+nan_df.rename(columns={0: 'nans'}, inplace=True)
+nan_df['has_nans'] = nan_df['nans'] > 0
+
+# boxplot
+sns.boxplot(x=nan_df['PCT_DIABETES_ADULTS13'], hue=nan_df['has_nans'])
+plt.show()
+
+# t test
+import scipy.stats as stats
+ttest = stats.ttest_ind(nan_df.loc[nan_df['has_nans']==True, 'PCT_DIABETES_ADULTS13'], nan_df.loc[nan_df['has_nans']==False, 'PCT_DIABETES_ADULTS13'])
+print(f'counties with nans have diabetes rates between {ttest.confidence_interval()[0]:.4f} and {ttest.confidence_interval()[1]:.4f} greater than counties without nans, p-value = {ttest[1]:.4e}')
+
+# remove rows with nans
+obj1_df.dropna(axis=0, inplace=True)
+
+## Nonlinearity
+yvars = ['PCT_DIABETES_ADULTS13', 'HDM', 'Life Expectancy']
+xvars = [v for v in obj1_df.columns if v not in yvars]
+sns.pairplot(obj1_df, x_vars=xvars, y_vars=yvars, diag_kind='kde')
+plt.show()
